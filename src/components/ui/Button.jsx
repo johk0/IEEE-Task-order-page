475394d7846089aa12fee502253ee7f3dbf2816f
Youssef Elaraby <y.ahmed.ea@gmail.com>
2024-09-20 20:58:59 +0300

    add button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,5 +1,11 @@
-const Button = () => {
-	return <div>Button</div>;
+const Button = ({ title }) => {
+	return (
+		<>
+			<button className="bg-primary text-black px-4 py-2 rounded-sm cursor-pointer hover:bg-black hover:text-white transition-all duration-300">
+				{title}
+			</button>
+		</>
+	);
 };
 
 export default Button;
--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -3,12 +3,13 @@ import MainTitle from "../../components/ui/MainTitle";
 import Title from "../../components/ui/title";
 import Description from "../../components/ui/description";
 import Menu from "../../components/menu/Menu";
+import Button from "../../components/ui/Button";
 import { menu } from "../../data";
 const Order = () => {
 	return (
 		<>
-			<section className="order-page bg-[#f7f2e9] w-full overflow-hidden pb-10">
-				<div className="container mx-auto lg:px-40">
+			<section className="order-page bg-[#f7f2e9] w-full overflow-hidden ">
+				<div className="container mx-auto lg:px-40 pb-10">
 					<div className="py-6">
 						<MainTitle title="FAST REACT PIZZA" />
 					</div>
@@ -21,6 +22,12 @@ const Order = () => {
 					</div>
 					<div>
 						<Menu data={menu} />
+					</div>
+					<div className="py-6">
+						<Description title="We're Open until 22.00" />
+					</div>
+					<div className="flex justify-center items-center pb-10">
+						<Button title="Order" />
 					</div>
 				</div>
 			</section>
